2a51c6c7fdc269029a11ada92883dcc6a796e6f7
danielsolomon332 <daniel.solomon332@gmail.com>
2022-03-02 14:27:15 -0700

    Add files and folders

---
+++ src/data/travelers-test-data.js
@@ -0,0 +1,11 @@
+const travelersTestData = [{
+    "id": 1,
+    "name": "Ham Leadbeater",
+    "travelerType": "relaxer",
+  }, {
+    "id": 2,
+    "name": "Rachael Vaughten",
+    "travelerType": "thrill-seeker",
+  }];
+
+export default travelersTestData;
